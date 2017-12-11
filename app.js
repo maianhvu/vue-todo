aa0fb6d17fce27b8ce497870a0e316a5ca23f81b
Mai Anh Vu <me@maianhvu.com>
2017-12-11 11:40:12 +0700

    add due date ability

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+const DateTime = luxon.DateTime
+
+const padWithZeroes = (number, length) => {
+    let result = number.toString()
+    if (result.length >= length) {
+        return result
+    }
+    let pad = ''
+    while (length > result.length) {
+        pad += '0';
+        --length
+    }
+    return pad + result
+}
+
 const app = new Vue({
     el: '#app',
     data: {
         todos: [],
-        pendingTodo: ''
+        pendingTodo: '',
+        pendingDeadline: ''
     },
     computed: {
         sortedTodos () {
@@ -12,10 +28,25 @@ const app = new Vue({
     },
     methods: {
         addTodo () {
-            this.todos.push({
+            let todo = {
                 text: this.pendingTodo,
                 done: false
-            })
+            }
+
+            if (this.pendingDeadline.trim().length &&
+                /^\d{1,2}:\d{2}\s\d{1,2}\/\d{1,2}\/\d{4}$/.test(this.pendingDeadline.trim())) {
+                
+                let [ time, date ] = this.pendingDeadline.trim().split(/\s/)
+                let [ hour, minute ] = time.split(':').map(n => parseInt(n))
+                let [ day, month, year ] = date.split('/').map(n => parseInt(n))
+                
+                let dateString = year + '-' + padWithZeroes(month, 2) +
+                    '-' + padWithZeroes(day, 2) + 'T' + padWithZeroes(hour, 2) + ':' +
+                    padWithZeroes(minute, 2)
+                todo.deadline = DateTime.local(year, month, day, hour, minute)
+            }
+
+            this.todos.push(todo)
             this.pendingTodo = ''
         },
 
@@ -25,6 +56,11 @@ const app = new Vue({
 
         remove (index) {
             this.todos.splice(index, 1)
+        },
+
+        formatDate (date) {
+            return date.toLocaleString(DateTime.DATETIME_FULL)
         }
+
     }
 })
